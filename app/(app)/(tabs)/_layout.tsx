f9e4281e0bc67df75003448861c879f45674eaf8
enkelm <enkel.murati33@gmail.com>
2025-01-06 17:43:41 +0100

    feat: add contact list

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,9 +1,19 @@
+import { Feather } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIconStyle: { position: 'absolute', top: '25%' },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{ tabBarIcon: ({ color }) => <Feather name="home" size={24} color={color} /> }}
+      />
     </Tabs>
   )
 }
--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,9 +1,36 @@
-import { Text, View } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { Link } from 'expo-router'
+import { FlatList, Text, View } from 'react-native'
+
+const Contact = ({ id, className }: { id: number; className?: string }) => {
+  return (
+    <Link href=".." asChild>
+      <View className={`flex-row items-center bg-primary gap-10 px-8 rounded-xl ${className}`}>
+        <Text className="border-4 border-background rounded-full text-background">
+          <Feather name="user" size={42} />
+        </Text>
+        <View className="flex justify-center h-full gap-1">
+          <Text className="text-background text-xl font-semibold">Enkel Murati</Text>
+          <Text className="text-background text-lg">
+            <Feather name="phone-call" size={16} className="text-background" />
+            {'  '}
+            +355 69 966 0867
+          </Text>
+          <Text className="text-background text-lg">
+            <Feather name="map-pin" size={16} className="text-background" />
+            {'  '}
+            Durrës
+          </Text>
+        </View>
+      </View>
+    </Link>
+  )
+}
 
 export default function Index() {
   return (
     <View className="p-safe flex flex-1 flex-col justify-center items-center bg-background">
-      <Text className="text-primary dark:text-lg">Edit app/index.tsx to edit this screen.</Text>
+      <FlatList data={new Array(10)} renderItem={({ index }) => <Contact id={index} className="h-[175px] mb-10" />} />
     </View>
   )
 }
