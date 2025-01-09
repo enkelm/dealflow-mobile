f9d8dc732d689131438f0960589899e8b6c57417
enkelm <enkel.murati33@gmail.com>
2025-01-09 23:36:27 +0100

    feat: add tab bar haptics and ios styling

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,12 +1,17 @@
 import { Feather } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
+import { HapticTab } from '@/components/ui/haptic-tab'
+import TabBarBackground from '@/components/ui/tab-bar-background'
+
 export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
         tabBarIconStyle: { position: 'absolute', top: '25%' },
       }}
     >
--- utils/navigation-theme.ts
+++ utils/navigation-theme.ts
@@ -1,36 +1,38 @@
-import { Theme, DefaultTheme } from '@react-navigation/native'
+import { Theme, DefaultTheme, DarkTheme } from '@react-navigation/native'
 
-const { fonts } = DefaultTheme
+// const { fonts } = DefaultTheme
 
 export const navigationThemes = new Map<'light' | 'dark', Theme>([
   [
     'light',
-    {
-      dark: false,
-      colors: {
-        primary: 'rgb(10, 132, 255)',
-        background: 'rgb(1, 1, 1)',
-        card: 'rgb(18, 18, 18)',
-        text: 'rgb(229, 229, 231)',
-        border: 'rgb(39, 39, 41)',
-        notification: 'rgb(255, 69, 58)',
-      },
-      fonts,
-    },
+    DefaultTheme,
+    // {
+    //   dark: false,
+    //   colors: {
+    //     primary: 'rgb(10, 132, 255)',
+    //     background: 'rgb(1, 1, 1)',
+    //     card: 'rgb(18, 18, 18)',
+    //     text: 'rgb(229, 229, 231)',
+    //     border: 'rgb(39, 39, 41)',
+    //     notification: 'rgb(255, 69, 58)',
+    //   },
+    //   fonts,
+    // },
   ],
   [
     'dark',
-    {
-      dark: true,
-      colors: {
-        primary: 'rgb(10, 132, 255)',
-        background: 'rgb(1, 1, 1)',
-        card: 'rgb(18, 18, 18)',
-        text: 'rgb(229, 229, 231)',
-        border: 'rgb(39, 39, 41)',
-        notification: 'rgb(255, 69, 58)',
-      },
-      fonts,
-    },
+    DarkTheme,
+    // {
+    //   dark: true,
+    //   colors: {
+    //     primary: 'rgb(10, 132, 255)',
+    //     background: 'rgb(1, 1, 1)',
+    //     card: 'rgb(18, 18, 18)',
+    //     text: 'rgb(229, 229, 231)',
+    //     border: 'rgb(39, 39, 41)',
+    //     notification: 'rgb(255, 69, 58)',
+    //   },
+    //   fonts,
+    // },
   ],
 ])
