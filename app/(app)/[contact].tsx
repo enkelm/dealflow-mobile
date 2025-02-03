e7a89535d1130b6462a851e73b0ed1084dc512ba
enkelm <enkel.murati33@gmail.com>
2025-02-03 17:51:33 +0100

    feat: make content scrollable

--- app/(app)/[contact].tsx
+++ app/(app)/[contact].tsx
@@ -1,6 +1,6 @@
 import { Feather } from '@expo/vector-icons'
-import { ComponentProps } from 'react'
-import { View, Text } from 'react-native'
+import { ComponentProps, useState } from 'react'
+import { View, Text, Pressable, ScrollView } from 'react-native'
 
 type DetailSectionItem = {
   icon?: ComponentProps<typeof Feather>['name']
@@ -15,7 +15,7 @@ type DetailSectionProps = {
 
 // ⁠⁠Deal
 
-const DetailSection = ({ name, items }: DetailSectionProps) => {
+const ContactSection = ({ name, items }: DetailSectionProps) => {
   return (
     <View className="flex flex-col bg-primary rounded-lg p-4">
       <Text className="text-background text-xl font-semibold">{name}</Text>
@@ -38,47 +38,73 @@ const DetailSection = ({ name, items }: DetailSectionProps) => {
   )
 }
 
+const ContactDetailsSection = () => {
+  const [expanded, setExpanded] = useState(false)
+
+  return (
+    <View className="flex flex-col bg-primary rounded-lg p-4">
+      <Text className="text-background text-xl font-semibold pb-1">Detajet</Text>
+      <Pressable onPress={() => setExpanded((e) => !e)}>
+        <View className="flex flex-col">
+          <Text className={`text-background text-md  ${!expanded ? 'max-h-24 truncate' : ''}`}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, repellat, et a tempore esse veniam enim
+            culpa nesciunt architecto possimus ratione est odio itaque hic neque sed. Architecto velit fugit dolores
+            blanditiis officia tempore soluta eligendi! Saepe quo ut pariatur ipsa earum maiores perspiciatis ipsum quas
+            aut reprehenderit mollitia perferendis, ullam rerum corporis unde, sint similique? Recusandae iste vitae,
+            debitis quae totam accusamus vero nisi tempora at sequi corrupti, atque perferendis eaque dolor quas. Autem
+            mollitia dolorum voluptas ea provident at inventore optio error animi quas dolor reprehenderit recusandae
+            aliquid doloremque nisi soluta, ad nemo impedit fugit deleniti voluptate sapiente.
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
+
 export default function Contact() {
   return (
-    <View className="pt-safe px-4 flex flex-1 bg-background gap-4">
-      <View className="flex flex-row justify-between items-center rounded-lg bg-primary p-4">
-        <View className="flex flex-col gap-2">
-          <Text className="text-background text-xl font-semibold">Enkel Murati</Text>
-          <Text className="text-background text-lg">
-            Mobile:
-            <Text className="text-background text-lg"> +355 69660867</Text>
-          </Text>
-        </View>
+    <View className="pt-safe px-4 flex flex-1 bg-background">
+      <ScrollView contentContainerClassName="gap-4">
+        <View className="flex flex-row justify-between items-center rounded-lg bg-primary p-4">
+          <View className="flex flex-col gap-2">
+            <Text className="text-background text-xl font-semibold">Enkel Murati</Text>
+            <Text className="text-background text-lg">
+              Mobile:
+              <Text className="text-background text-lg"> +355 69660867</Text>
+            </Text>
+          </View>
 
-        <Text className="border-4 border-background rounded-full text-background">
-          <Feather name="user" size={42} />
-        </Text>
-      </View>
-      <View className="flex flex-col bg-primary rounded-lg p-4">
-        <Text className="text-background text-xl font-semibold pb-1">Detajet</Text>
-        <View className="flex flex-col">
-          <Text className="text-background text-md">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum animi eveniet tempore reiciendis
-            exercitationem ea officia illum hic amet ipsam eum, maiores officiis, sed, saepe repudiandae molestiae
-            corrupti ex cumque?
+          <Text className="border-4 border-background rounded-full text-background">
+            <Feather name="user" size={42} />
           </Text>
         </View>
+        <ContactDetailsSection />
+        <ContactSection
+          name="Financat"
+          items={[
+            { icon: 'credit-card', name: 'Buxheti', value: '€ 140 000' },
+            { name: 'Financimi', value: 'Liquid' },
+          ]}
+        />
+        <ContactSection
+          name="Preferencat"
+          items={[
+            { icon: 'map', name: 'Zona', value: 'Currila' },
+            { icon: 'home', name: 'Siperfaqja', value: '120 m²' },
+            { icon: 'layout', name: 'Nr. i Dhomave', value: '2 + 1' },
+          ]}
+        />
+      </ScrollView>
+      <View className="absolute bottom-0 left-0 right-0 pb-safe z-10 bg-background">
+        <View className="flex flex-row justify-around items-center py-4">
+          <Pressable className="flex flex-col justify-center items-center">
+            <Feather name="edit" size={24} />
+          </Pressable>
+          <Pressable className="flex flex-col justify-center items-center">
+            <Feather name="trash" size={24} />
+          </Pressable>
+        </View>
       </View>
-      <DetailSection
-        name="Financat"
-        items={[
-          { icon: 'credit-card', name: 'Buxheti', value: '€ 140 000' },
-          { name: 'Financimi', value: 'Liquid' },
-        ]}
-      />
-      <DetailSection
-        name="Preferencat"
-        items={[
-          { icon: 'map', name: 'Zona', value: 'Currila' },
-          { icon: 'home', name: 'Siperfaqja', value: '120 m²' },
-          { icon: 'layout', name: 'Nr. i Dhomave', value: '2 + 1' },
-        ]}
-      />
     </View>
   )
 }
