c756314c37e5bf7152ca6a3b20a811b1714408f8
enkelm <enkel.murati33@gmail.com>
2025-02-04 19:57:31 +0100

    feat: add react-hook-form

--- app/(app)/[contact]/edit.tsx
+++ app/(app)/[contact]/edit.tsx
@@ -1,9 +1,49 @@
 import { View, Text, TextInput } from 'react-native'
+import { Controller, useForm } from 'react-hook-form'
 
 export default function EditModal() {
+  const { control, handleSubmit, formState } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+    },
+  })
   return (
     <View className="p-8 flex flex-1 flex-col">
-      <TextInput />
+      <Controller
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput placeholder="First name" onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        name="firstName"
+      />
+
+      <Controller
+        control={control}
+        rules={{ maxLength: 100 }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput placeholder="Last name" onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        name="lastName"
+      />
+
+      <Controller
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            keyboardType="phone-pad"
+            dataDetectorTypes="phoneNumber"
+            placeholder="Phone number"
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+        )}
+        name="phoneNumber"
+      />
     </View>
   )
 }
